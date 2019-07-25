00a177ab8267cfd1091399baef9339c8b8fe47a1
Emil Dimitrov <edimitrov1@gmail.com>
2019-07-25 15:49:04 +0300

    Accept a players array instead of individual players

--- lib/Round.rb
+++ lib/Round.rb
@@ -1,8 +1,6 @@
 class Round
-  def initialize(p1, p2)
-    @p1 = p1
-    @p2 = p2
-    @active_player = p1
+  def initialize(players)
+    @players = players
     @board = Board.new
   end
   
@@ -10,7 +8,7 @@ class Round
   # by asking each player successively for 
   # input, with a stop condition if there is a winner
   def play
-    loop do
+    loop do 
       board.draw
       turn(by: active_player)
 
