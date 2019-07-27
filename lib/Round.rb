dba278828ef73009f16be603f23fc608d4433e04
Emil Dimitrov <edimitrov1@gmail.com>
2019-07-27 23:57:49 +0300

    Refactor show winner

--- lib/Round.rb
+++ lib/Round.rb
@@ -8,9 +8,9 @@ class Round
   # by asking each player successively for 
   # input, with a stop condition if there is a winner
   def play
-    loop do 
+    until game_over? 
       board.draw
-      turn(by: players.first)
+      turn(player: players.first)
 
       if active_player.won?(
         positions: board.extract_player_positions(sign: active_player.sign),
@@ -22,37 +22,43 @@ class Round
         break
       end
 
-      if board.full?
-        board.draw
-        print_draw
-        break
-      end
-
       players.reverse!
     end
+    complete
   end
 
   private
 
   attr_accessor :players
 
-  def turn(by:)
+  def game_over?
+    full? || won?
+  end
+
+  def full?
+    board.full?
+  end
+
+  def won?
+    board.won?
+  end
+
+  def turn(player:)
     pos = nil
     loop do
-      pos = by.get_input
+      pos = player.get_input
       break if board.valid_position?(position: pos)
     end
     board.add_mark(mark: by.sign, position: pos)
   end
 
+  def complete
+    puts "No winner. Board is full." if full?
+    show_winner(players.last) if won?
   end
 
   # Shows who won the particular round in the console 
   def print_winner(player)
     puts "#{player.name} has won the round."
-  end
-
-  def print_draw
-    puts "No winner"
   end
 end
