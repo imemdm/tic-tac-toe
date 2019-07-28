72c1acbad91c34cb432577537d3a9fb64d59a984
Emil Dimitrov <edimitrov1@gmail.com>
2019-07-28 22:59:32 +0300

    Switch to class methods in InputHandler

--- lib/InputHandler.rb
+++ lib/InputHandler.rb
@@ -11,16 +11,16 @@ class InputHandler
   private
 
   # Returns an integer if input is numeric, nil otherwise
-  def convert_input(input)
+  def self.convert_input(input)
     input.to_i - 1 if numeric?(input)
   end
 
   # A helper that checks if a given string represents an integer
-  def numeric?(str)
+  def self.numeric?(str)
     Integer(str) != nil rescue false
   end
 
-  def ask(player: )
+  def self.ask(player: )
     print "#{player.name}, mark a position on the board (1-9): "
   end
 end
