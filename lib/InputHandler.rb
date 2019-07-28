300b6e45b444c3b434ecbb7cde32dea6b8b7c3c4
Emil Dimitrov <edimitrov1@gmail.com>
2019-07-28 22:44:15 +0300

    Can get valid input from a player

--- lib/InputHandler.rb
+++ lib/InputHandler.rb
@@ -1,10 +1,24 @@
 class InputHandler
-  def self.get(by:)
-    ask(player: by)
-    
+  def self.get(from:)
+    input = nil
+    until convert_input(input)
+      ask(player: from)
+      input = gets.chomp
+    end
+    input
   end
 
   private
+
+  # Returns an integer if input is numeric, nil otherwise
+  def convert_input(input)
+    input.to_i if numeric?(input)
+  end
+
+  # A helper that checks if a given string represents an integer
+  def numeric?(str)
+    Integer(str) != nil rescue false
+  end
 
   def ask(player: )
     print "#{player.name}, mark a position on the board (0-8): "
--- lib/Player.rb
+++ lib/Player.rb
@@ -8,31 +8,7 @@ class Player
     @score = 0
   end
 
-  # Get input from the command line
-  def get_input
-    ask_for_input
-    convert_input(gets.chomp)
-  end
-
-  # Returns an integer if input is numeric, nil otherwise
-  def convert_input(input)
-    if numeric?(input)
-      input.to_i
-    else
-      nil
-    end
-  end
-
   def increase_score
     self.score += 1
   end
-
-  private
-
-  # A helper that checks if a given string represents an integer
-  def numeric?(str)
-    Integer(str) != nil rescue false
-  end
-
-
 end
