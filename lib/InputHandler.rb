81dbddebbc88a04391fb123d6809bf35ef029b4e
Emil Dimitrov <edimitrov1@gmail.com>
2019-07-28 21:53:43 +0300

    Improve input ask

--- lib/InputHandler.rb
+++ lib/InputHandler.rb
@@ -1,11 +1,12 @@
 class InputHandler
-  def self.get
-    ask_for_input
+  def self.get(by:)
+    ask(player: by)
+    
   end
 
   private
 
-  def ask_for_input
-    print "#{self.name}, mark a position on the board (0-8): "
+  def ask(player: )
+    print "#{player.name}, mark a position on the board (0-8): "
   end
 end
